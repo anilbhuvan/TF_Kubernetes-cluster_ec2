27e252ef0b151958bf2304af257c54ad8b566917
anilbhuvan <anilbhuvan1116@gmail.com>
2023-05-20 14:07:07 -0500

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 terraform {
-    region = "us-east-1"  # Replace with your desired region
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
-  
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
