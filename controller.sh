c3f7212014087b982dec8551a6adb04e50f1a0f8
Anil Bhuvan <109484863+anilbhuvan@users.noreply.github.com>
2023-06-18 18:40:00 -0500

    Update controller.sh

--- controller.sh
+++ controller.sh
@@ -80,7 +80,7 @@ sudo bash script.sh
 # Installing kubeadm, kubelet and kubectl
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
