28496001688a03767ec56636e32c9ecd7070ac3c
Anil Bhuvan <109484863+anilbhuvan@users.noreply.github.com>
2023-06-18 18:54:05 -0500

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -79,7 +79,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl restart kubelet
 sudo systemctl daemon-reload
