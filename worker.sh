b135e688d2ff75c11d0f1b8e03cc355ec6bf1df3
Anil Bhuvan <109484863+anilbhuvan@users.noreply.github.com>
2023-06-18 18:40:30 -0500

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -76,7 +76,7 @@ touch /home/ubuntu/"configured-75%"
 # Installing kubeadm, kubelet and kubectl
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
